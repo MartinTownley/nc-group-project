ecbc1785a0ddc256ae2936b0c8c4c53d279e22d6
martin <martinrtownley@gmail.com>
2023-03-24 11:00:51 +0000

    update dependencies

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text> How about this?</Text>
+      <Text>Testing</Text>
       <StatusBar style="auto" />
     </View>
   );
