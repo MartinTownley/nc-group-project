dcb80e909f6bad8a75fc4fb5ebe8f087f34b04d6
martin <martinrtownley@gmail.com>
2023-03-24 12:16:09 +0000

    add some example components

--- App.js
+++ App.js
@@ -1,12 +1,31 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  View,
+  Text,
+  Image,
+  ScrollView,
+  TextInput,
+  StyleSheet,
+} from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Testing</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView>
+      <Text> Some text</Text>
+      <View>
+        <Text> Some more text</Text>
+        <Image
+          sourc={{
+            uri: "https://www.google.com/url?sa=i&url=https%3A%2F%2Funsplash.com%2Fs%2Fphotos%2Fcool-stuff&psig=AOvVaw03qHrfFzh9vafokl89Z3j7&ust=1679743558708000&source=images&cd=vfe&ved=0CA8QjRxqFwoTCPCO8ci69P0CFQAAAAAdAAAAABAD",
+          }}
+          style={{ width: 200, height: 200 }}
+        />
+      </View>
+      <TextInput
+        style={{ height: 40, borderColor: "gray", borderWidth: 1 }}
+        defaultView="Type here"
+      />
+    </ScrollView>
   );
 }
 
